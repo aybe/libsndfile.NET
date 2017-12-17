9db6cf9aa50152e0f0cc6c56a20b845ea3367367
Aybe <aybe@users.noreply.github.com>
2017-12-17 07:16:45 +0100

    Fixed calling conventions

--- libsndfile.NET/SfVirtualRead.cs
+++ libsndfile.NET/SfVirtualRead.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace libsndfile.NET
 {
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate long SfVirtualRead(IntPtr ptr, long count, IntPtr userData);
 }
--- libsndfile.NET/SfVirtualTell.cs
+++ libsndfile.NET/SfVirtualTell.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace libsndfile.NET
 {
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate long SfVirtualTell(IntPtr userData);
 }
